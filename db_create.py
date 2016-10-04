53a4090145882eafe301413a8c40a7fa875b0540
Anne <axu@yahoo.com>
2016-10-03 20:54:12 -0400

    second

---
+++ db_create.py
@@ -0,0 +1,6 @@
+import sqlite3
+
+conn = sqlite3.connect('montreal.sqlite')
+c = conn.cursor()
+
+c.execute()
--- getlinks.py
+++ getlinks.py
@@ -1,13 +1,26 @@
 import requests
 import codecs
+from lxml import html
+import sqlite3
+
+sqlite_file = 'montreal.sqlite'
+tlink = 'tlink'
+
+conn = sqlite3.connect(sqlite_file)
+c = conn.cursor
+
 
 base = 'http://www.iu91.com/content/listRS?page='
 
-for i in range(1,40):
+for i in range(2,4):
     murl = base + str(i)
     r = requests.get(murl)
-    fname = 'page_'+str(i)
+    fname = 'page_'+str(i)+'.html'
     f= codecs.open(fname,'w',encoding='utf8')
     f.write(r.text)
     f.close()
+    tree = html.fromstring(r.content)
+    rlinks = tree.xpath('//span[@class="title_move"]/a/@href')
 
+print rlinks
+print len(rlinks)
