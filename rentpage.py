05a08a586d15f903a3d436efd0d1634c73d682d9
Anne <axu@yahoo.com>
2016-10-04 18:45:04 -0400

    11

--- rentpage.py
+++ rentpage.py
@@ -4,6 +4,8 @@ import requests
 import codecs
 from lxml import html
 import sqlite3
+#(u'/rs/rent/2016-01/40839.shtml',)
+#encoded = [[s.encode('utf8') for s in t] for t in resultsList]
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -17,7 +19,7 @@ c = conn.cursor()
 base = 'http://www.iu91.com'
 
 conn.text_factory = bytes
-plinks = c.execute("select link from plinks where id=100")
+plinks = c.execute("select link from plinks where id<20")
 for plink in plinks:
     slink  = str(plink)
     llink = list(slink)
@@ -41,6 +43,9 @@ for plink in plinks:
     address = tree.xpath('//div[@class="detailinfosubtitle"]/text()')
     waddress = " ".join(address[0].split())
     
+    #rent = tree.xpath('//div[@id="detailpage_left_side"]/div[1]/ul/li[1]/text()')
+    #wrent = " ".join(rent[1].split())
+    #.//*[@id='detailpage_left_side']/div[1]/ul/li[3]/span 
     for item in tree.xpath('//div[@id="detailpage_left_side"]/div[1]/ul/li'):
         tt = item.xpath('./span/text()')
         print tt[0]
@@ -74,12 +79,32 @@ for plink in plinks:
            wenv =  " ".join(pp[1].split())
         if u'附近地铁' in tt[0]:
            wmetro =  " ".join(pp[1].split())
+        #if u'每月租金' in tt[0]:
+          # wrent =  " ".join(pp[1].split())
+   
+    #rent = tree.xpath('//div[@id="detailpage_left_side"]/div[1]/ul/li[1]/text()')
+    #desc
+    #rent = tree.xpath('//div[@id="detailpage_left_side"]/div[1]/ul/li[1]/text()')
+    #oname
+    #rent = tree.xpath('//div[@id="detailpage_left_side"]/div[1]/ul/li[1]/text()')
+    #phone
+    #rent = tree.xpath('//div[@id="detailpage_left_side"]/div[1]/ul/li[1]/text()')
+    #email
+    #rent = tree.xpath('//div[@id="detailpage_left_side"]/div[1]/ul/li[1]/text()')
+    #print title[0]
+    #print waddress
+    #print wrent
+    #print rstyle[1],method[1],rooms[1],length[1],intime[1]
+    #print wstyle,wmethod,wrooms,wlength,wintime,wtenant,wcondition,wequip,wenv
     
-        #html/body/div[5]/div[1]/div/div[1]/span[2]
-        rentsale = tree.xpath('html/body/div[5]/div[1]/div/div[1]/span[1]')
-        if u'出租' in rentsale:
-           print 'for rent: ',rentsale
-
-        c.execute('insert into pageinfo ("title","address","rent","rstyle","method","rooms","length","intime","condition","equip","env","metro") values (?,?,?,?,?,?,?,?,?,?,?,?)',(title[0],waddress,wrent,wstyle,wmethod,wrooms,wlength,wintime,wcondition,wequip,wenv,wmetro)) 
-#conn.commit()  
+    rentsale = tree.xpath('//div[@class="title"]/span[1]/text()')
+    #print rentsale
+    house = tree.xpath('//div[@class="title"]/span[2]/text()')
+    
+    if u'出租' in rentsale:
+       print 'for rent: ',rentsale[0]
+       if u'住宅' in house:
+          print 'house:',house[0]
+          c.execute('insert into rentinfo ("title","address","rent","rstyle","method","rooms","length","intime","condition","equip","env","metro") values (?,?,?,?,?,?,?,?,?,?,?,?)',(title[0],waddress,wrent,wstyle,wmethod,wrooms,wlength,wintime,wcondition,wequip,wenv,wmetro)) 
+conn.commit()  
 conn.close()
