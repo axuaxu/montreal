2aa17183eaa52b72008606c6ccfd1e0a3b62a6f5
Anne <axu@yahoo.com>
2016-10-04 05:52:29 -0400

    7

--- rentinfo.py
+++ rentinfo.py
@@ -34,10 +34,38 @@ for plink in plinks:
     r = requests.get(purl)
     tree = html.fromstring(r.content)
     title = tree.xpath('//span[@class="detail_top_title_text"]/text()')
-    #stitle = str(title).decode('utf-8')
-    #dtitle = stitle.decode('gb18030').decode('utf-8')
-    print title[0]
+    address = tree.xpath('//div[@class="detailinfosubtitle"]/text()')
+    waddress = " ".join(address[0].split())
+    #rent = tree.xpath('//div[@class="detailinfostart"]/ul/li[1]/text()')
+    rent = tree.xpath('//div[@id="detailpage_left_side"]/div[1]/ul/li[1]/text()')
+    wrent = " ".join(rent[1].split())
     
-    c.execute('insert into pageinfo ("title") values (?)',(title[0],)) 
+    rstyle = tree.xpath('//div[@id="detailpage_left_side"]/div[1]/ul/li[2]/text()')
+    wstyle = " ".join(rstyle[1].split())
+    method = tree.xpath('//div[@id="detailpage_left_side"]/div[1]/ul/li[3]/text()')
+    wmethod = " ".join(method[1].split())
+    
+    rooms = tree.xpath('//div[@id="detailpage_left_side"]/div[1]/ul/li[5]/text()')
+    wrooms = " ".join(rooms[1].split())
+    length = tree.xpath('//div[@id="detailpage_left_side"]/div[1]/ul/li[6]/text()')
+    wlength = " ".join(length[1].split())
+    #length
+    intime = tree.xpath('//div[@id="detailpage_left_side"]/div[1]/ul/li[7]/text()')
+    wintime = " ".join(intime[1].split())
+    #rent = tree.xpath('//div[@id="detailpage_left_side"]/div[1]/ul/li[1]/text()')
+    #desc
+    #rent = tree.xpath('//div[@id="detailpage_left_side"]/div[1]/ul/li[1]/text()')
+    #oname
+    #rent = tree.xpath('//div[@id="detailpage_left_side"]/div[1]/ul/li[1]/text()')
+    #phone
+    #rent = tree.xpath('//div[@id="detailpage_left_side"]/div[1]/ul/li[1]/text()')
+    #email
+    #rent = tree.xpath('//div[@id="detailpage_left_side"]/div[1]/ul/li[1]/text()')
+    print title[0]
+    print waddress
+    print wrent
+    print rstyle[1],method[1],rooms[1],length[1],intime[1]
+    print wstyle,wmethod,wrooms,wlength,wintime
+    #c.execute('insert into pageinfo ("title") values (?)',(title[0],)) 
 conn.commit()  
 conn.close()
