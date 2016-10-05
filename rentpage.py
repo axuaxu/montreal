bdd723393f4a197e077ccbe23d419e04f5095f23
Anne <axu@yahoo.com>
2016-10-04 21:55:21 -0400

    13

--- rentpage.py
+++ rentpage.py
@@ -31,7 +31,7 @@ wenv = ''
 wmetro = ''
 
 conn.text_factory = bytes
-plinks = c.execute("select link from plinks where id<30")
+plinks = c.execute("select link from plinks where id=25")
 rows = plinks.fetchall()
 for plink in rows:
     slink  = str(plink)
@@ -62,27 +62,20 @@ for plink in rows:
     address = tree.xpath('//div[@class="detailinfosubtitle"]/text()')
     waddress = " ".join(address[0].split())
     
-    #rent = tree.xpath('//div[@id="detailpage_left_side"]/div[1]/ul/li[1]/text()')
-    #wrent = " ".join(rent[1].split())
-    #.//*[@id='detailpage_left_side']/div[1]/ul/li[3]/span 
     for item in tree.xpath('//div[@id="detailpage_left_side"]/div[1]/ul/li'):
         tt = item.xpath('./span/text()')
         if not tt:
            print 'empty list'
            continue
-        print tt[0]
         pp = item.xpath('./text()')
-        #print pp[0]
         #print pp[1] 
         try:
             detail = " ".join((tt[0]+pp[1]).split())
             #print detail
         except IndexError:
             continue
-        #if '每月租金'.decode('latin1').encode('utf8') in tt[0]:
         if u'每月租金' in tt[0]:
            wrent =  " ".join(pp[1].split())
-           print 'rent is ', wrent
         if u'房屋类型' in tt[0]:
            wstyle =  " ".join(pp[1].split())
         if u'出租方式' in tt[0]:
@@ -103,12 +96,14 @@ for plink in rows:
            wenv =  " ".join(pp[1].split())
         if u'附近地铁' in tt[0]:
            wmetro =  " ".join(pp[1].split())
-        #if u'每月租金' in tt[0]:
-          # wrent =  " ".join(pp[1].split())
    
-    #rent = tree.xpath('//div[@id="detailpage_left_side"]/div[1]/ul/li[1]/text()')
-    #desc
-    #rent = tree.xpath('//div[@id="detailpage_left_side"]/div[1]/ul/li[1]/text()')
+    ldesc = tree.xpath('//div[@id="summary"]/text()')
+    j  = 0
+    desc = ''
+    while j<len(ldesc):
+         desc = desc + ldesc[j]
+         j= j+1
+    print 'description: ',desc
     #oname
     #rent = tree.xpath('//div[@id="detailpage_left_side"]/div[1]/ul/li[1]/text()')
     #phone
@@ -120,7 +115,6 @@ for plink in rows:
     #print wrent
     #print rstyle[1],method[1],rooms[1],length[1],intime[1]
     #print wstyle,wmethod,wrooms,wlength,wintime,wtenant,wcondition,wequip,wenv
-    print wrent   
     rentsale = tree.xpath('//div[@class="title"]/span[1]/text()')
     #print rentsale
     house = tree.xpath('//div[@class="title"]/span[2]/text()')
@@ -129,7 +123,6 @@ for plink in rows:
        print 'for rent: ',rentsale[0]
        if u'住宅' in house:
           print 'house:',house[0]
-          print wrent
-          c.execute('insert into rentinfo ("title","address","rent","rstyle","method","rooms","length","intime","condition","equip","env","metro") values (?,?,?,?,?,?,?,?,?,?,?,?)', (title[0],waddress,wrent,wstyle,wmethod,wrooms,wlength,wintime,wcondition,wequip,wenv,wmetro,)) 
+          c.execute('insert into rentinfo ("title","address","rent","rstyle","method","rooms","length","intime","condition","equip","env","metro","desc") values (?,?,?,?,?,?,?,?,?,?,?,?,?)', (title[0],waddress,wrent,wstyle,wmethod,wrooms,wlength,wintime,wcondition,wequip,wenv,wmetro,desc)) 
           conn.commit()  
 conn.close()
