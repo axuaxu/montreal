81ebd8c06dc65609c9849059b783cca03aa01b7d
Anne <axu@yahoo.com>
2016-10-06 16:48:41 -0400

    20

--- db_create.py
+++ db_create.py
@@ -13,3 +13,9 @@ CREATE TABLE "pageinfo" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQ
 
 
 CREATE TABLE "rentinfo" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "num" INTEGER, "title" CHAR, "address" CHAR, "rent" CHAR, "rstyle" CHAR, "method" CHAR, "rooms" CHAR, "length" CHAR, "intime" CHAR, "desc" TEXT, "oname" CHAR, "phone" CHAR, "email" CHAR, "tenant" CHAR, "condition" CHAR, "equip" CHAR, "env" CHAR, "nearby" CHAR, "metro" CHAR)
+
+CREATE TABLE "rentpage" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "num" INTEGER, "title" CHAR, "address" CHAR, "rent" CHAR, "rstyle" CHAR, "method" CHAR, "rooms" CHAR, "length" CHAR, "intime" CHAR, "desc" TEXT, "oname" CHAR, "phone" CHAR, "email" CHAR, "tenant" CHAR, "condition" CHAR, "equip" CHAR, "env" CHAR, "nearby" CHAR, "metro" CHAR)
+
+
+CREATE TABLE "monrent" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "num" INTEGER, "udate" CHAR, "rlink" CHAR,"title" CHAR, "address" CHAR, "rent" CHAR, "rstyle" CHAR, "method" CHAR, "rooms" CHAR, "length" CHAR, "intime" CHAR, "desc" TEXT, "tenant" CHAR, "treq" CHAR, "condition" CHAR, "equip" CHAR, "env" CHAR, "bus" CHAR, "metro" CHAR, "hway" CHAR, "oname" CHAR, "phone" CHAR,  "phone2" CHAR, "email" CHAR,"wechat" CHAR,"qq" CHAR )
+
--- monrent.py
+++ monrent.py
@@ -13,6 +13,19 @@ from lxml import html
 import sqlite3
 import re
 
+def setFields(udate,surl,sparser,c):
+    num = surl.rsplit('/',1)[1]
+    num = num.split('.',1)[0]
+    print num       
+    title = sparser.xpath('//span[@class="detail_top_title_text"]/text()')[0]
+    print num,title
+    #pass
+
+sqlite_file = "montreal.sqlite"
+conn = sqlite3.connect(sqlite_file)
+conn.text_factory = bytes
+c = conn.cursor()
+
 murl = 'http://www.iu91.com/rs/rent'
 
 driver = webdriver.Firefox()
@@ -30,9 +43,9 @@ today = datetime.now()
 udate = ''
 base = 'http://www.iu91.com'
 
-for i in range(3,5):
+for i in range(3,4):
     
-    for j in range(15,17):
+    for j in range(15,16):
         xlink = '//*[@id="listArea"]/ul/li['+str(j)+']/div/div[2]/div[1]/span/a/@href'
         rlink = parser.xpath(xlink)
         print j,rlink[0]
@@ -50,11 +63,14 @@ for i in range(3,5):
            udate = str(today+relativedelta(days=-int(m)))[:10]
         print udate
 
-        second_driver = webdriver.Firefox()    
-        second_driver.get(base+rlink[0])
+        second_driver = webdriver.Firefox()
+        surl = base+rlink[0]    
+        second_driver.get(surl)
 
-        second_parser = html.fromstring(driver.page_source,driver.current_url)
-          
+        second_parser = html.fromstring(second_driver.page_source,second_driver.current_url)
+        
+        setFields(udate,surl,second_parser,c)
+  
         second_driver.close()
  
     time.sleep(10)
@@ -70,3 +86,5 @@ for i in range(3,5):
     time.sleep(10)
     parser = html.fromstring(driver.page_source,driver.current_url)
 driver.close()
+conn.commit()
+conn.close()
