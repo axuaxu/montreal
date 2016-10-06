9009921a00b87f6dc0232f42136576eb24d7b0ce
Anne <axu@yahoo.com>
2016-10-06 08:45:45 -0400

    20

--- monrent.py
+++ monrent.py
@@ -26,40 +26,47 @@ elem.send_keys(Keys.RETURN)
 
 parser = html.fromstring(driver.page_source,driver.current_url)
 
-#rlink = parser.xpath('//*[@id="listArea"]/ul/li[1]/div/div[2]/div[1]/span/a/text()')
-#print rlink[0]
-rlink = parser.xpath('//*[@id="listArea"]/ul/li[1]/div/div[2]/div[1]/span/a/@href')
-print rlink[0]
-utime = parser.xpath('//*[@id="listArea"]/ul/li[1]/div/div[2]/div[5]/text()')
-print utime[0]
-udate = ''
 today = datetime.now()
-if u'当天更新' in utime[0]:
-   udate = str(today)[:10]
-if u'昨天更新' in utime[0]:
-   udate = str(today+relativedelta(days=-1))[:10]
-if u'前天更新' in utime[0]:
-   udate = str(today+relativedelta(days=-2))[:10]
-m = re.search("\d",utime[0])
-if m:
-   udate = str(today+relativedelta(days=-int(m)))[:10]
-print udate
-
-#driver.find_element_by_xpath("//*[@id='listArea']/ul/li[1]/div/div[2]/div[1]/span/a").click()
-
+udate = ''
 base = 'http://www.iu91.com'
-second_driver = webdriver.Firefox()    
-second_driver.get(base+rlink[0])
 
-second_parser = html.fromstring(driver.page_source,driver.current_url)
-rlink = parser.xpath('//*[@id="listArea"]/ul/li[1]/div/div[2]/div[1]/span/a/@href')
-#window_after = driver.window_handles
+for i in range(3,5):
+    
+    for j in range(15,17):
+        xlink = '//*[@id="listArea"]/ul/li['+str(j)+']/div/div[2]/div[1]/span/a/@href'
+        rlink = parser.xpath(xlink)
+        print j,rlink[0]
+        xtime = '//*[@id="listArea"]/ul/li['+str(j)+']/div/div[2]/div[5]/text()'
+        utime = parser.xpath(xtime)
+        print utime[0]
+        if u'当天更新' in utime[0]:
+           udate = str(today)[:10]
+        if u'昨天更新' in utime[0]:
+           udate = str(today+relativedelta(days=-1))[:10]
+        if u'前天更新' in utime[0]:
+           udate = str(today+relativedelta(days=-2))[:10]
+        m = re.search("\d",utime[0])
+        if m:
+           udate = str(today+relativedelta(days=-int(m)))[:10]
+        print udate
 
-#driver.switch_to_window(window_after[0])
-time.sleep(15)
-second_driver.close()
-#driver.switch_to_window(window_before[0])
+        second_driver = webdriver.Firefox()    
+        second_driver.get(base+rlink[0])
 
-#driver.find_element_by_xpath("//*[@id='listArea']/ul/li[2]/div/div[2]/div[1]/span/a").click()
+        second_parser = html.fromstring(driver.page_source,driver.current_url)
+          
+        second_driver.close()
+ 
+    time.sleep(10)
+    #.//*[@id='pagination']/a[4]
+    xpage = '//*[@id="pagination"]/a['+str(i+2)+']/@href'
+    rpage = parser.xpath(xpage)
+    print i,'next page',rpage[0]    
+   
+    #driver.get(base+rpage[0])
 
-#driver.close()
+    #elem = driver.find_element_by_class("nextPage")
+    driver.find_element_by_xpath(".//*[@id='pagination']/a[16]").click()
+    time.sleep(10)
+    parser = html.fromstring(driver.page_source,driver.current_url)
+driver.close()
